bc4167212361eddac8eb357cd7ab9919b1e63492
Mike Bromberek <mikebromberek@gmail.com>
2022-11-17 06:04:06 -0600

    Extract amount from a sentence

---
+++ ch04/extracting.py
@@ -0,0 +1,22 @@
+import spacy
+nlp = spacy.load('en_core_web_sm')
+
+# works with $, £, or €. Would have to be changed if $ comes after amount
+doc = nlp(u'The firm earned £1.5 million in 2017.')
+phrase = ''
+
+for token in doc:
+    print(token.text, token.pos_, token.tag_, spacy.explain(token.pos_))
+print('\n')
+
+# extrace phrase for amount of money
+for token in doc:
+    if token.tag_ == '$':
+        phrase = token.text
+        i = token.i+1
+        while doc[i].tag_ == 'CD':
+            phrase += doc[i].text + ' '
+            i+=1
+        break
+phrase = phrase[:-1]
+print(phrase)
